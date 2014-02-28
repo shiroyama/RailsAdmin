44023dfba6f177c8893f6f31f40a25129e03fd22
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-02-28 07:59:24 +0000

    rails new RailsAdmin -d mysql --skip-bundle & git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsAdmin::Application.config.secret_key_base = 'f0a0a157f7fcbf9d2ccc8f0f7797734c469d9526a1de259c9bbb4eabe9b85baa1d81becfc5d0f3a2976a1d4693677c8d58bd51be3b64cad12cb453f0f7cbdd90'
